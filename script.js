0f086b6999e84cbdc96511edf128f9cb59781221
Artur <arturhs2019@gmail.com>
2025-01-10 19:02:33 -0300

    Add the funcion add

--- script.js
+++ script.js
@@ -8,7 +8,9 @@
 // });
 
 function add(num1, num2){
-    return num1 + num2;
+    Number(num1);
+    Number(num2);
+    return     Number(num1) + Number(num2);
 }
 
 function subtract(num1, num2){
@@ -28,3 +30,30 @@ function operate(num1,operator,num2){
         return add(num1,num2);  
     }
 }
+
+let acumulador = 1;
+const display = document.querySelector(".display");
+const buttons = document.querySelectorAll(".buttons button:not(.add)");
+const addButton = document.querySelector(".add")
+
+
+let num1;
+let num2;
+
+// Adiciona eventos de clique a todos os botões
+buttons.forEach(button => {
+    button.addEventListener("click", () => {
+        // Concatena o número clicado ao que já está no display
+        display.innerHTML += button.textContent;
+    });
+});
+
+addButton.addEventListener("click",() => {
+        if(!(num1)){
+        num1 = display.innerHTML;
+        display.innerHTML = "";}
+        else{
+            num2 = display.innerHTML;
+            display.innerHTML = add(num1,num2);
+        }
+})
