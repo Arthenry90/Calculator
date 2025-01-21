4ba8d5bf43e9a9e37d4127d16fcede9c89f1ae0f
Artur <arturhs2019@gmail.com>
2025-01-20 21:42:26 -0300

    Add divide, multiply, subtract and clear buttons

--- script.js
+++ script.js
@@ -14,6 +14,8 @@ function add(num1, num2){
 }
 
 function subtract(num1, num2){
+    Number(num1);
+    Number(num2);
     return num1 - num2;
 }
 
@@ -26,19 +28,51 @@ function divide(num1, num2){
 }
 
 function operate(num1,operator,num2){
-    if(operator == "+"  ){
-        return add(num1,num2);  
-    }
+  switch (operator){
+    case "+":
+        console.log( "lalou")
+        return add(num1,num2);
+    case "-":
+        return multiply(num1,num2);
+    case "x":
+        return multiply(num1,num2);
+    case "/":
+        return divide(num1,num2);
+  }
 }
 
 let acumulador = 1;
-const display = document.querySelector(".display");
-const buttons = document.querySelectorAll(".buttons button:not(.add)");
-const addButton = document.querySelector(".add")
-
-
 let num1;
 let num2;
+let operator;
+
+const display = document.querySelector(".display");
+const buttons = document.querySelectorAll(".buttons button:not(.add, .subtract, .divide, .multiply, .operate, .clear)");
+const addButton = document.querySelector(".add");
+const subtractButton = document.querySelector(".subtract");
+const multiplyButton = document.querySelector(".multiply");
+const divideButton = document.querySelector(".divide");
+const operateButton = document.querySelector(".operate");
+const clearButton = document.querySelector(".clear");
+
+
+clearButton.addEventListener("click", () => {
+    num1 = "";
+    num2 = "";
+    operator = "";
+    display.innerHTML = "";
+})
+
+operateButton.addEventListener("click", () =>{
+    if(!(num1)){
+        console.log("pq clicou jegue");
+    }else{
+        num2 = display.innerHTML;
+        display.innerHTML = operate(num1,operator,num2);
+    }
+} )
+
+
 
 // Adiciona eventos de clique a todos os botões
 buttons.forEach(button => {
@@ -51,9 +85,59 @@ buttons.forEach(button => {
 addButton.addEventListener("click",() => {
         if(!(num1)){
         num1 = display.innerHTML;
-        display.innerHTML = "";}
+        operator = "+";
+        display.innerHTML = ""; }
         else{
             num2 = display.innerHTML;
             display.innerHTML = add(num1,num2);
         }
+})
+
+subtractButton.addEventListener("click",() => {
+    if(!(num1)){
+    num1 = display.innerHTML;
+    operator = "-";
+    console.log(num1);
+    display.innerHTML = "";}
+    else{
+
+        num2 = display.innerHTML;
+        console.log(num2);
+
+        display.innerHTML = subtract(num1,num2);
+        console.log(num1 - num2);
+
+    }
+})
+multiplyButton.addEventListener("click",() => {
+    if(!(num1)){
+    num1 = display.innerHTML;
+    operator = "x";
+    console.log(num1);
+    display.innerHTML = "";}
+    else{
+
+        num2 = display.innerHTML;
+        console.log(num2);
+
+        display.innerHTML = multiply(num1,num2);
+        console.log(num1 - num2);
+
+    }
+})
+divideButton.addEventListener("click",() => {
+    if(!(num1)){
+    num1 = display.innerHTML;
+    console.log(num1);
+    operator = "/";
+    display.innerHTML = "";}
+    else{
+
+        num2 = display.innerHTML;
+        console.log(num2);
+
+        display.innerHTML = divide(num1,num2);
+        console.log(num1 - num2);
+
+    }
 })
